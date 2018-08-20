0e854f2d500b08c93bded7b2be566fc8654b469f
andrejlevkovitch <andrejlevkivitch@gmail.com>
2018-08-20 15:39:39 +0300

    небольшая модификация, которая позволяет подключаться только одному клиенту к серверу

--- socket/sources/cs/cs.cpp
+++ socket/sources/cs/cs.cpp
@@ -28,24 +28,28 @@ CS::CS(::QWidget *parent)
       line_layout->addWidget(line_edit);
       line_layout->addWidget(send_button);
     }
+    auto connect_wgt = new ::QWidget;
     auto connecting_layout = new ::QHBoxLayout;
     {
       auto ip_edit = new ::QLineEdit;
       connect(ip_edit, SIGNAL(returnPressed()), this, SLOT(connect_slot()));
 
       auto connect_button = new ::QPushButton{"connect"};
-      connect(connect_button, SIGNAL(clicked()), ip_edit, SIGNAL(returnPressed()));
+      connect(connect_button, SIGNAL(clicked()), ip_edit,
+              SIGNAL(returnPressed()));
 
       auto listen_button = new ::QPushButton{"listen"};
       connect(listen_button, SIGNAL(clicked()), this, SLOT(listen_slot()));
+      connect(listen_button, SIGNAL(clicked()), connect_wgt, SLOT(hide()));
 
       connecting_layout->addWidget(ip_edit);
       connecting_layout->addWidget(connect_button);
       connecting_layout->addWidget(listen_button);
     }
+    connect_wgt->setLayout(connecting_layout);
     general_layout->addWidget(text_edit_);
     general_layout->addLayout(line_layout);
-    general_layout->addLayout(connecting_layout);
+    general_layout->addWidget(connect_wgt);
   }
   this->setLayout(general_layout);
 }
@@ -60,13 +64,13 @@ void CS::send_message() {
   line_edit->clear();
 }
 
-void CS::read_message() { text_edit_->append("NM: " + socket_->readAll()); }
+void CS::read_message() { text_edit_->append("UM: " + socket_->readAll()); }
 
 void CS::listen_slot() {
   if (!server_) {
     text_edit_->append("__SERVER__");
     server_ = new ::QTcpServer{this};
-    server_->listen(/*::QHostAddress::AnyIPv4*/::QHostAddress::Any, default_port);
+    server_->listen(::QHostAddress::Any, default_port);
     connect(server_, SIGNAL(newConnection()), this, SLOT(set_socket()));
   }
 }
@@ -85,10 +89,14 @@ void CS::connect_slot() {
 }
 
 void CS::set_socket() {
-  text_edit_->append("CONNECTED");
-  socket_ = server_->nextPendingConnection();
-  connect(socket_, SIGNAL(disconnected()), socket_, SLOT(deleteLater()));
-  connect(socket_, &::QTcpSocket::disconnected, text_edit_,
-          [=]() { text_edit_->append("DISCONNECTED"); });
-  connect(socket_, SIGNAL(readyRead()), this, SLOT(read_message()));
+  if (!socket_) {
+    text_edit_->append("CONNECTED");
+    socket_ = server_->nextPendingConnection();
+    connect(socket_, SIGNAL(disconnected()), socket_, SLOT(deleteLater()));
+    connect(socket_, &::QTcpSocket::disconnected, text_edit_,
+            [=]() { text_edit_->append("DISCONNECTED"); });
+    connect(socket_, SIGNAL(readyRead()), this, SLOT(read_message()));
+  } else {
+    text_edit_->append("CONNECTION ATTEMPT");
+  }
 }
