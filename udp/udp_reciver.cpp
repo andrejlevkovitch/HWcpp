fbd3e8a68e71cd3e4c6fec0c52a9e48f26a73dc8
andrejlevkovitch <andrejlevkovitch@gmail.com>
2018-12-02 17:40:58 +0300

    fix problems with udp connection

--- udp/udp_reciver.cpp
+++ udp/udp_reciver.cpp
@@ -13,20 +13,27 @@ udp_reciver::udp_reciver(QWidget *parent)
       port_number_{new QSpinBox{this}} {
   this->setWindowTitle("UDP reciver");
   port_number_->setMaximum(100000);
+  adress_edit_->setPlaceholderText("Ip");
   auto v_layout = new QVBoxLayout{this};
   v_layout->addWidget(text_browser_);
   v_layout->addWidget(port_number_);
   v_layout->addWidget(adress_edit_);
   connect(adress_edit_, &QLineEdit::returnPressed, this, [=]() {
-    socket_->bind(::QHostAddress{adress_edit_->text()}, port_number_->value());
+    if (socket_->bind(::QHostAddress{adress_edit_->text()},
+                      port_number_->value())) {
+      text_browser_->append("bind to: " + adress_edit_->text() + ":" +
+                            QString::number(port_number_->value()));
+    } else {
+      text_browser_->append("fail bind to: " + adress_edit_->text() + ":" +
+                            QString::number(port_number_->value()));
+    }
   });
-  connect(socket_, &QUdpSocket::readyRead, text_browser_, [=]() {
+  connect(socket_, &QUdpSocket::readyRead, this, [=]() {
     text_browser_->append("\nnew message: ");
     std::string message;
     message.resize(1000);
     socket_->readDatagram(&message[0], 1000);
     text_browser_->append(QString(message.c_str()));
-    text_browser_->append("\n");
   });
   this->setLayout(v_layout);
 }
--- udp/udp_sender.cpp
+++ udp/udp_sender.cpp
@@ -13,16 +13,27 @@ udp_sender::udp_sender(QWidget *parent)
       text_browser_{new QTextBrowser} {
   this->setWindowTitle("UDP sender");
   port_number_->setMaximum(100000);
+  adress_edit_->setPlaceholderText("Ip");
+  message_->setPlaceholderText("Message");
   auto v_layout = new QVBoxLayout{this};
   v_layout->addWidget(text_browser_);
   v_layout->addWidget(port_number_);
   v_layout->addWidget(adress_edit_);
   v_layout->addWidget(message_);
+  connect(adress_edit_, &QLineEdit::returnPressed, this, [=]() {
+    socket_->connectToHost(adress_edit_->text(), port_number_->value(),
+                           ::QIODevice::WriteOnly);
+    // socket_->bind(::QHostAddress{adress_edit_->text()},
+    // port_number_->value());
+  });
   connect(message_, &QLineEdit::returnPressed, this, [=]() {
-    socket_->writeDatagram(
-        message_->text().toStdString().c_str(), message_->text().size(),
-        ::QHostAddress{adress_edit_->text()}, port_number_->value());
-    text_browser_->append("\nsend message: " + message_->text());
+    int n = socket_->write(message_->text().toStdString().c_str());
+    if (n != -1) {
+      text_browser_->append("\nsend message: " + message_->text() + ":" +
+                            QString::number(n));
+    } else {
+      text_browser_->append("fail send message");
+    }
   });
   this->setLayout(v_layout);
 }
